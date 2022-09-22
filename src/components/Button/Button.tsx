654502d9e3346d420b49f73666bfa0aa95e7a406
maricoIR <matin.kahrizi@gmail.com>
2022-09-22 10:14:15 +0330

    changed interface to type

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-interface ButtonProps {
+type ButtonProps = {
   label: string;
-}
+};
 
 const Button = (props: ButtonProps) => {
   return <button>{props.label}</button>;
